35bd4a3efea539eb60f7a8e031f0eee94b39ddae
Max <maxime.delannoy@outlook.com>
2020-11-19 16:38:24 +0100

    footer added

--- navigii-prosperitate/src/components/HomePage.jsx
+++ navigii-prosperitate/src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import "./UserReview.css";
 import "./HomePage.css";
+import Footer from "./Footer";
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -31,6 +32,7 @@ class HomePage extends React.Component {
             bon fumet !
           </p>
         </div>
+        <Footer />
       </div>
     );
   }
