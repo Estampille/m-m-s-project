8b064fbf38aadfb3c34624683560d760d9c449c3
Max <maxime.delannoy@outlook.com>
2020-11-19 12:51:35 +0100

    composant added

---
+++ navigii-prosperitate/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>This is the contact Component</p>;
+  }
+}
---
+++ navigii-prosperitate/src/components/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+
+render(){
+  return(
+
+  );
+}
+
+
+}
+
+export default HomePage;
